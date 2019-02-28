057c0407b674ffbb4d19242d0461da5a8b0837cc
Adam George <afgeorge2@gmail.com>
2019-02-28 15:13:01 -0500

    route config

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace Vidly
                     "MoviesByReleaseDate",
                     "movies/released/{year}/{month}",
                     new { controller = "Movies", action = "ByReleaseDate"},
-                    new {year =@"\d{4}", month = @"\d{2}"}
+                    new {year =@"2015|2016", month = @"\d{2}"}
                 );
             routes.MapRoute(
                 name: "Default",
